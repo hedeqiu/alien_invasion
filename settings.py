39b00c581936bbee5a4d05d3ec8f393be5425cd5
hedeqiu <hedeqiu487@163.com>
2018-06-05 20:46:34 +0800

    change bullet num

--- settings.py
+++ settings.py
@@ -13,4 +13,4 @@ class Settings():
         self.bullet_width = 2
         self.bullet_height = 10
         self.bullet_color = 60, 60, 60
-        self.bullets_allowed = 3
+        self.bullets_allowed = 5
