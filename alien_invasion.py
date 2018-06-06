7712644225521e80fff0f18d7be992827654a2b1
hedeqiu <hedeqiu487@163.com>
2018-06-06 18:01:13 +0800

    update 20180606

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,6 +4,8 @@ from pygame.sprite import Group
 
 from settings import Settings
 from ship import Ship
+from alien import Alien
+from game_stats import GameStats
 
 def run_game():
     #init pygame / setting /screen
@@ -12,14 +14,22 @@ def run_game():
     screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
+    stats = GameStats(ai_settings)
+
     ship = Ship(ai_settings,screen)
     bullets = Group()
+
+    aliens = Group()
+    gf.create_fleet(ai_settings, screen, ship, aliens)
+
 
     #start game main loop
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)
-        ship.update()
-        gf.update_bullets(bullets)
-        gf.update_screen(ai_settings, screen, ship, bullets)
+        if stats.game_active:
+            ship.update()
+            gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
+            gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 run_game()
--- settings.py
+++ settings.py
@@ -3,14 +3,23 @@ class Settings():
     def __init__(self):
         #init game setting
         #screen setting
-        self.screen_width=1024
-        self.screen_height=640
+        self.screen_width=1080
+        self.screen_height=650
         self.bg_color=(230,230,230)
+
+        #ship settins
         self.ship_speed_factor = 1.5
+        self.ship_limit = 3
 
         #bullet setting
-        self.bullet_speed_factor = 1
-        self.bullet_width = 2
+        self.bullet_speed_factor = 3
+        self.bullet_width = 3
         self.bullet_height = 10
         self.bullet_color = 60, 60, 60
-        self.bullets_allowed = 5
+        self.bullets_allowed = 3
+
+        #alien setting
+        self.alien_speed_factor = 1
+        self.fleet_drop_speed = 10
+        self.fleet_direction = 1
+
